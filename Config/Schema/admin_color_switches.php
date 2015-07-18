aacde84b76e9723413a46bc766c962cc8fa8be86
arata <contact@materializing.net>
2015-07-18 15:23:43 +0900

    fixed スキーマ更新

--- Config/Schema/admin_color_switches.php
+++ Config/Schema/admin_color_switches.php
@@ -16,6 +16,7 @@ class AdminColorSwitchesSchema extends CakeSchema {
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary', 'comment' => 'ID'),
 		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false, 'comment' => 'ユーザーID'),
 		'color_code' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'カラーコード', 'charset' => 'utf8'),
+		'flg_footer' => array('type' => 'boolean', 'null' => true, 'default' => '0', 'comment' => 'フッター色変更指定'),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '更新日'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => '作成日'),
 		'indexes' => array(
